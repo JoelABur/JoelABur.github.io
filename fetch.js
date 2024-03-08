e6e91960dfde82cea705674353c6391091f2474f
JoelABur <joelalexender.burns@calbaptist.edu>
2024-03-07 17:26:18 -0800

    test

--- fetch.js
+++ fetch.js
@@ -1,16 +1,31 @@
-const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c18eed165bmsh2a15e18504749ffp18e97ejsn0546bcdfba19',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
+// const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c18eed165bmsh2a15e18504749ffp18e97ejsn0546bcdfba19',
+// 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+// 	}
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+    "method": 'GET',
+    "headers": {
+        "X-RapidAPI-Key": "c18eed165bmsh2a15e18504749ffp18e97ejsn0546bcdfba19",
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
+    }
+})
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.log(err);
+});
